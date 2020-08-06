456e22ba0cadb7b8052391ce617caf027573bb4f
Stephane Landelle <slandelle@gatling.io>
2020-08-06 21:13:34 +0200

    Update default JVM options and System props
    Motivation:
    
    In Gatling 3.4.0, we are fixing our set of JVM options and System props.
    
    Modifications:
    
    Use the same set as gatling.sh, gatling.bat, gatling-maven-plugin, gatling-sbt and FrontLine.
    
    Result:
    
    Better default JVM tuning for better performance

--- src/main/groovy/io/gatling/gradle/JvmConfigurable.groovy
+++ src/main/groovy/io/gatling/gradle/JvmConfigurable.groovy
@@ -3,21 +3,19 @@ package io.gatling.gradle
 trait JvmConfigurable {
 
     static final List<String> DEFAULT_JVM_ARGS = [
+        '-server',
         '-Xmx1G',
         '-XX:+HeapDumpOnOutOfMemoryError',
         '-XX:+UseG1GC',
-        '-XX:MaxGCPauseMillis=30',
-        '-XX:G1HeapRegionSize=16m',
-        '-XX:InitiatingHeapOccupancyPercent=75',
         '-XX:+ParallelRefProcEnabled',
-        '-XX:+PerfDisableSharedMem',
-        '-XX:+OptimizeStringConcat'
+        '-XX:MaxInlineLevel=20',
+        '-XX:MaxTrivialSize=12',
+        '-XX:-UseBiasedLocking'
     ]
 
-    static final Map DEFAULT_SYSTEM_PROPS = ["java.net.preferIPv4Stack": true, "java.net.preferIPv6Addresses": false]
+    static final Map DEFAULT_SYSTEM_PROPS = ["java.net.preferIPv6Addresses": true]
 
     List<String> jvmArgs
 
     Map systemProperties
-
 }
