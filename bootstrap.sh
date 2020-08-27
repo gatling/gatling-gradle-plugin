389f6b73b5e070301edd72a5395869bcebfaa9fa
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2020-08-27 12:34:50 +0300

    feat: generate gitignore for sample project from central repository

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,28 @@ cp -R src/main/bootstrap/* ..
 cd ..
 rm -rf tmp
 
+rm -f .gitignore || true
+
+echo '#' >> .gitignore
+echo '# From https://github.com/github/gitignore/blob/master/Gradle.gitignore' >> .gitignore
+echo '#' >> .gitignore
+curl -sL https://raw.githubusercontent.com/github/gitignore/master/Gradle.gitignore >> .gitignore
+
+echo '#' >> .gitignore
+echo '# From https://github.com/github/gitignore/blob/master/Java.gitignore' >> .gitignore
+echo '#' >> .gitignore
+curl -sL https://raw.githubusercontent.com/github/gitignore/master/Java.gitignore >> .gitignore
+
+echo '#' >> .gitignore
+echo '# From https://github.com/github/gitignore/blob/master/Global/JetBrains.gitignore' >> .gitignore
+echo '#' >> .gitignore
+curl -sL https://raw.githubusercontent.com/github/gitignore/master/Global/JetBrains.gitignore >> .gitignore
+
+echo '#' >> .gitignore
+echo '# From https://github.com/github/gitignore/blob/master/Global/Eclipse.gitignore' >> .gitignore
+echo '#' >> .gitignore
+curl -sL https://raw.githubusercontent.com/github/gitignore/master/Global/Eclipse.gitignore >> .gitignore
+
 echo
 echo 'Created sample Gatling project for Gradle.'
 echo 'Use following to run sample simulations.'
