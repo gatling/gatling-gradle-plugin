5f8a74b4881d14b67130df15c4e833e9d2a297fa
tpetillot <th.petillot@gmail.com>
2021-09-03 11:41:50 -0400

    chore: align plugins command names
    Motivation:
    Ease users comprehension
    
    Modifications:
    * Update publish reference to uplod
    
    Result:
    Task name is now gatlingEnterpriseUpload
    
    Ref: MISC-243

--- src/main/groovy/io/gatling/gradle/GatlingEnterpriseUploadTask.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingEnterpriseUploadTask.groovy
@@ -7,7 +7,7 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.TaskAction
 
 @CacheableTask
-class GatlingEnterprisePublishTask extends DefaultTask {
+class GatlingEnterpriseUploadTask extends DefaultTask {
 
     @TaskAction
     void publish() {
