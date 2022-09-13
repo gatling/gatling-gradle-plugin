76cf58a54cec555248d19c9c7663bde6809fbc38
Stephane Landelle <slandelle@gatling.io>
2022-09-13 20:51:49 +0200

    Upgrade Gatling default version: 3.8.4

--- src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
@@ -264,7 +264,7 @@ class GatlingPluginExtension implements JvmConfigurable {
 
     static final String RESOURCES_DIR = "src/gatling/resources"
 
-    static final String GATLING_VERSION = '3.8.3'
+    static final String GATLING_VERSION = '3.8.4'
 
     static final String SCALA_VERSION = '2.13.8'
 
