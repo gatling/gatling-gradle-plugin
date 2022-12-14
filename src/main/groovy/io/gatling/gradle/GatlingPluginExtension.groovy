8c840a8f03a9bf384037634085d1dc6d4c96df27
Stephane Landelle <slandelle@gatling.io>
2022-12-14 15:11:38 +0100

    chore: upgrade Scala default value to 2.13.10

--- src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
@@ -267,7 +267,7 @@ class GatlingPluginExtension {
 
     static final String GATLING_VERSION = '3.9.0'
 
-    static final String SCALA_VERSION = '2.13.8'
+    static final String SCALA_VERSION = '2.13.10'
 
     static final Closure DEFAULT_SIMULATIONS = { include("**/*Simulation*.java", "**/*Simulation*.kt", "**/*Simulation*.scala") }
 
