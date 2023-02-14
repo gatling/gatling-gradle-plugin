41e12aafaa20845f40d0d17fbca9a17c37a6b3fd
Guillaume Galy <ggaly@gatling.io>
2023-02-14 16:02:26 +0100

    chore: Add compatibility with Gradle 8.0
    Motivation:
    
    Gradle 8.0 has been released and it removes the classifier field from the archive tasks' API, which we were using.
    
    Modifications:
    
    - Create and use a compatibility adapter to use the old classifier field on Gradle < 5.2, and use its replacement property on newer versions
    - Add Gradle 8.0 to the test suite
    
    Result:
    
    Plugin is now compatible with Gradle 5.0 to 8.0

--- src/test/groovy/helper/GatlingFuncSpec.groovy
+++ src/test/groovy/helper/GatlingFuncSpec.groovy
@@ -29,5 +29,5 @@ abstract class GatlingFuncSpec extends GatlingSpec {
         createRunner(gradleArgs).build()
     }
 
-    protected static final List<String> SUPPORTED_GRADLE_VERSIONS = ["5.0", "5.6.4", "6.0", "6.3", "6.4.1", "6.9.1", "7.0", "7.4.2"]
+    protected static final List<String> SUPPORTED_GRADLE_VERSIONS = ["5.0", "5.6.4", "6.0", "6.3", "6.4.1", "6.9.1", "7.0", "7.6", "8.0"]
 }
