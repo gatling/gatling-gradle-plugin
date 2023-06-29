98e9e4fcdf57e5aeb2f54ae855b3d81aed7d0a11
Mahatma_Fatal_Error <MahatmaFatalError@users.noreply.github.com>
2023-06-29 14:18:19 +0200

    remove deprecated -target (#78)
    * remove jdk specification, use default instead

--- src/main/groovy/io/gatling/gradle/GatlingPlugin.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingPlugin.groovy
@@ -196,7 +196,6 @@ final class GatlingPlugin implements Plugin<Project> {
         project.tasks.named("compileGatlingScala").configure {
             scalaCompileOptions.with {
                 additionalParameters = [
-                    "-target:jvm-1.8",
                     "-deprecation",
                     "-feature",
                     "-unchecked",
