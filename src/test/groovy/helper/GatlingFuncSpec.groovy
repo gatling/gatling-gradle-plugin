93861f0082f1b4d329545b354614c86b1f7234c0
Stephane Landelle <slandelle@gatling.io>
2023-08-10 13:15:44 +0200

    chore: update gradle version compatibility matrix (#81)

--- src/test/groovy/helper/GatlingFuncSpec.groovy
+++ src/test/groovy/helper/GatlingFuncSpec.groovy
@@ -29,5 +29,5 @@ abstract class GatlingFuncSpec extends GatlingSpec {
         createRunner(gradleArgs).build()
     }
 
-    protected static final List<String> SUPPORTED_GRADLE_VERSIONS = ["5.0", "5.6.4", "6.0", "6.3", "6.4.1", "6.9.1", "7.0", "7.6", "8.0"]
+    protected static final List<String> SUPPORTED_GRADLE_VERSIONS = ["5.0", "5.6.4", "6.0", "6.3", "6.4.1", "6.9.4", "7.0", "7.6.2", "8.0", "8.2.1"]
 }
