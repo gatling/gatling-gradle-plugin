c7dd1ae8fbff8af86f5456491175c896f492b709
slandelle <slandelle@gatling.io>
2023-12-18 11:36:40 +0100

    chore: upgrade Scala 2.13.12

--- src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
@@ -301,7 +301,7 @@ class GatlingPluginExtension {
 
     static final String GATLING_VERSION = '3.9.5'
 
-    static final String SCALA_VERSION = '2.13.11'
+    static final String SCALA_VERSION = '2.13.12'
 
     static final Closure DEFAULT_SIMULATIONS = { include("**/*Simulation*.java", "**/*Simulation*.kt", "**/*Simulation*.scala") }
 
