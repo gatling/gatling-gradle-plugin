24e5238de8445188ec1f77778216816fc63b4ff2
slandelle <slandelle@gatling.io>
2024-02-14 11:40:37 +0100

    chore: remove useless options passed to Gatling#main
    Motivation:
    
    We don't need to pass the compiled code folder nor the resources one. These files are already in the classpath.

--- src/main/groovy/io/gatling/gradle/GatlingRunTask.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingRunTask.groovy
@@ -48,23 +48,11 @@ class GatlingRunTask extends DefaultTask {
     }
 
     List<String> createGatlingArgs(String gatlingVersion) {
-
-        FileCollection classesDirs = project.sourceSets.gatling.output.classesDirs
-
-        File javaClasses = classesDirForLanguage(classesDirs, 'java')
-        File scalaClasses = classesDirForLanguage(classesDirs, 'scala')
-        File kotlinClasses = classesDirForLanguage(classesDirs, 'kotlin')
-        File binariesFolder = scalaClasses != null ? scalaClasses :
-            kotlinClasses != null ? kotlinClasses : javaClasses
-
         def gatlingVersionComponents = gatlingVersion.split("\\.")
         int gatlingMajorVersion = Integer.valueOf(gatlingVersionComponents[0])
         int gatlingMinorVersion = Integer.valueOf(gatlingVersionComponents[1])
 
-        def baseArgs = [
-            '-bf', binariesFolder.absolutePath,
-            "-rsf", "${project.sourceSets.gatling.output.resourcesDir}",
-            "-rf", gatlingReportDir.absolutePath]
+        def baseArgs = ["-rf", gatlingReportDir.absolutePath]
 
         return (gatlingMajorVersion == 3 && gatlingMinorVersion >= 8) || gatlingMajorVersion >= 4 ?
             baseArgs + ["-l", "gradle", "-btv", GradleVersion.current().version] :
