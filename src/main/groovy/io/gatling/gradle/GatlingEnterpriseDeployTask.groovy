f27e54bfdfbf2525b442194be3f9e5f939e5c31c
Bastien <bamar@gatling.io>
2024-03-21 11:11:37 +0100

    feat: add gatlingEnterpriseDeploy target HYB-515
    Motivation:
    Make accessible new package descriptor feature
    
    Modifications:
    Add GatlingEnterpriseDeployTask

---
+++ src/main/groovy/io/gatling/gradle/GatlingEnterpriseDeployTask.groovy
@@ -0,0 +1,24 @@
+package io.gatling.gradle
+
+import io.gatling.plugin.BatchEnterprisePlugin
+import io.gatling.plugin.deployment.DeploymentConfiguration
+import io.gatling.plugin.model.BuildTool
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class GatlingEnterpriseDeployTask extends DefaultTask {
+
+    @TaskAction
+    void deploy() {
+        final GatlingPluginExtension gatlingPlugin = project.extensions.getByType(GatlingPluginExtension)
+        final String version = project.version.toString()
+        final BatchEnterprisePlugin enterprisePlugin = gatlingPlugin.enterprise.initBatchEnterprisePlugin(version, logger)
+
+        final File descriptorFile = DeploymentConfiguration.fromBaseDirectory(project.rootDir)
+        final File packageFile = inputs.files.singleFile
+        final Boolean isPrivateRepositoryEnabled = gatlingPlugin.enterprise.controlPlaneUrl != null
+        final String artifactId = project.name
+        final String implementationVersion = (getClass().getPackage().getImplementationVersion() == null) ? "undefined" : getClass().getPackage().getImplementationVersion()
+        enterprisePlugin.deployFromDescriptor(descriptorFile, packageFile, artifactId, isPrivateRepositoryEnabled, BuildTool.GRADLE, implementationVersion)
+    }
+}
