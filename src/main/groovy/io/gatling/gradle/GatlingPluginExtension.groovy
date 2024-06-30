b56c4c2799d73e6a9549a0539ac62d8548da81b3
slandelle <slandelle@gatling.io>
2024-06-30 20:13:59 +0200

    feat: upgrade Gatling 3.11.5

--- src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingPluginExtension.groovy
@@ -289,7 +289,7 @@ class GatlingPluginExtension {
 
     static final String GATLING_RESOURCES_DIR = "src/gatling/resources"
 
-    static final String GATLING_VERSION = '3.11.4'
+    static final String GATLING_VERSION = '3.11.5'
 
     static final String SCALA_VERSION = '2.13.14'
 
