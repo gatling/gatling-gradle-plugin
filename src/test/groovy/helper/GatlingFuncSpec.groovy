ba100e0e4efac341ac0ba6a89d0548975b625e27
Sébastien BREVET <sbrevet@gatling.io>
2024-11-13 10:57:51 +0100

    fix: URL in lowercase

--- src/test/groovy/helper/GatlingFuncSpec.groovy
+++ src/test/groovy/helper/GatlingFuncSpec.groovy
@@ -5,7 +5,7 @@ import org.gradle.testkit.runner.GradleRunner
 
 abstract class GatlingFuncSpec extends GatlingSpec {
 
-    static def GATLING_HOST_NAME_SYS_PROP = "-Dgatling.hostName=HTTP://COMPUTER-DATABASE.GATLING.IO"
+    static def GATLING_HOST_NAME_SYS_PROP = "-Dgatling.hostName=http://computer-database.gatling.io"
 
     void prepareGroovyTestWithScala(String fixtureDir) {
         createBuildFolder(fixtureDir, SimulationLanguage.SCALA)
