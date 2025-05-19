6aed6937b47c96b664d96a1851d9d65150b57458
tpetillot <tpetillot@gatling.io>
2025-05-19 17:01:39 +0200

    chore: handle simulation validation ID on deploy command
    Motivation:
    Validate a given simulation ID have been deployed.
    
    Ref: CLD-6861

--- src/main/groovy/io/gatling/gradle/GatlingEnterpriseDeployTask.groovy
+++ src/main/groovy/io/gatling/gradle/GatlingEnterpriseDeployTask.groovy
@@ -17,8 +17,11 @@ class GatlingEnterpriseDeployTask extends DefaultTask {
         final File descriptorFile = DeploymentConfiguration.fromBaseDirectory(project.rootDir, gatlingPlugin.enterprise.packageDescriptorFilename)
         final File packageFile = inputs.files.singleFile
         final Boolean isPrivateRepositoryEnabled = gatlingPlugin.enterprise.controlPlaneUrl != null
+        final String validateSimulationId = gatlingPlugin.enterprise.validateSimulationId;
         final String artifactId = project.name
 
-        deploymentInfo = enterprisePlugin.deployFromDescriptor(descriptorFile, packageFile, artifactId, isPrivateRepositoryEnabled)
+        deploymentInfo = validateSimulationId != null
+            ? enterprisePlugin.deployFromDescriptor(descriptorFile, packageFile, artifactId, isPrivateRepositoryEnabled, validateSimulationId)
+            : enterprisePlugin.deployFromDescriptor(descriptorFile, packageFile, artifactId, isPrivateRepositoryEnabled)
     }
 }
